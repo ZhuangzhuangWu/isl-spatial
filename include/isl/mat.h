9cce001c6f50bc4144c50e326abdb61cc442bac1
Oleksandr Zinenko <ftynse@gmail.com>
2017-01-11 13:44:04 +0100

    Make isl_mat_rank public

--- include/isl/mat.h
+++ include/isl/mat.h
@@ -90,6 +90,7 @@ __isl_give isl_mat *isl_mat_add_zero_rows(__isl_take isl_mat *mat, unsigned n);
 void isl_mat_col_add(__isl_keep isl_mat *mat, int dst_col, int src_col);
 
 struct isl_mat *isl_mat_unimodular_complete(struct isl_mat *M, int row);
+int isl_mat_rank(__isl_keep isl_mat *M);
 
 __isl_give isl_mat *isl_mat_from_row_vec(__isl_take isl_vec *vec);
 __isl_give isl_mat *isl_mat_concat(__isl_take isl_mat *top,
