70cfe26681ba34f2a7982373dd9e1e07e34cb87e
Oleksandr Zinenko <ftynse@gmail.com>
2017-04-10 13:17:36 +0200

    Linear independent completion for matrices

--- include/isl/mat.h
+++ include/isl/mat.h
@@ -95,6 +95,8 @@ __isl_give isl_mat *isl_mat_unimodular_complete(__isl_take isl_mat *M, int row);
 __isl_give isl_mat *isl_mat_row_basis(__isl_take isl_mat *mat);
 __isl_give isl_mat *isl_mat_row_basis_extension(
 	__isl_take isl_mat *mat1, __isl_take isl_mat *mat2);
+__isl_give isl_mat *isl_mat_linear_independent_complete(__isl_take isl_mat *mat,
+	int upto);
 int isl_mat_rank(__isl_keep isl_mat *M);
 
 __isl_give isl_mat *isl_mat_from_row_vec(__isl_take isl_vec *vec);
